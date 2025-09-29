2dc75dc339c7e3b938efcccafaeac4e955a30ff7
Elkous <Iminogbar@gmail.com>
2025-09-29 20:39:50 +0100

    first commit

---
+++ ap.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// هادي تخلي CSS/JS/صور يخدمو مباشرة
+app.use(express.static(__dirname));
+
+// Routes مخصصة
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/about.html');
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/contact.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+
+
+// باش Express يعرف ملفات CSS و JS
+//app.use(express.static(__dirname)); // هنا نخلي كل الملفات فالـ root متاحة
+
+//app.get('/', (req, res) => {
+ // res.sendFile(path.join(__dirname, 'index.html'));
+//});
+
+//app.listen(port, () => {
+ // console.log(`السيرفر خدام على http://localhost:${port}`);
+//});
+
+
+
---
+++ myapp-backend/myapp-frontend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// Route بسيطة
+app.get('/', (req, res) => {
+  res.json({ message: 'سلام من السيرفر 🚀' });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
